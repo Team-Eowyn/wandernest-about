636b8f91fb565f8c2558bddf10d41ef5384b80f0
Yuri Kim <yurikim123@gmail.com>
2020-05-14 08:03:37 -1000

    fixed rating meaning so that it corresponds with bubbles

--- client/src/components/HotelLinks.jsx
+++ client/src/components/HotelLinks.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import styled from 'styled-components';
 import Title from '../styledComponents/Title.js';
+import {Web} from 'styled-icons/foundation';
+import {DiagonalArrowRightUp} from 'styled-icons/evaicons-solid';
+// @styled-icons/evaicons-outline/DiagonalArrowRightUpOutline
 
-const LinksContainer = styled.div`
+const LinkContainer = styled.div`
   grid-column: 1 / 3;
   padding: 10px 0 0;
+`;
+const HotelWebsite = styled.div`
+  display: flex;
+  align-items: center
+`;
+const LinkIcon = styled(Web)`
+  color: #4a4a4a;
+`;
+const Link = styled.span`
+  font-family: 'LatoRegular';
+  font-size: 14px;
+  color: #4a4a4a;
+`;
+const ArrowIcon = styled(DiagonalArrowRightUp)`
+  color: #4a4a4a;
 `;
 
 class HotelLinks extends React.Component {
@@ -17,9 +35,14 @@ class HotelLinks extends React.Component {
 
   render() {
     return (
-      <LinksContainer>
+      <LinkContainer>
         <Title>Hotel links</Title>
-      </LinksContainer>
+        <HotelWebsite>
+          <LinkIcon size='20px' />
+          <Link>Visit hotel website</Link>
+          <ArrowIcon size='20px' />
+        </HotelWebsite>
+      </LinkContainer>
     );
   }
 }
