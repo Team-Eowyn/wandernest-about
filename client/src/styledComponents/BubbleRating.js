c77f82f0ded3b544717b13bf8962c415c50b6a02
Yuri Kim <yurikim123@gmail.com>
2020-05-14 11:07:44 -1000

    added click functionality

---
+++ client/src/styledComponents/BubbleRating.js
@@ -0,0 +1,85 @@
+import styled from 'styled-components';
+import React from 'react';
+
+const RatingBox = styled.span`
+  display: flex;
+  flex-direction: column;
+  padding: 10px 0px 0;
+`;
+
+const RatingMeaning = styled.div`
+  font-family: 'LatoBold';
+  font-size: 16px;
+`;
+const RatingBubbles = styled.span`
+  display: flex;
+  flex-direction: row;
+  margin: -10px 0 0 -2px;
+  align-items: baseline;
+`;//center
+
+const RatingBubble = styled.span`
+  --percent: calc(${props => props.rating} / 5 * 100%);
+  display: inline-block;
+  font-size: 37px ;
+  padding-right: 7px;
+
+  &::before {
+    content: '●●●●●';
+    letter-spacing: -2px;
+    background: linear-gradient(90deg, ${props => props.bubbleBackground} var(--percent), ${props => props.bubbleColor} var(--percent));
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    -webkit-text-stroke: 1px #00aa6c;
+  }
+`;
+//only works if content is black circle (can't be white/ medium or big)
+
+const RatingTotal = styled.span`
+  font-family: 'LatoRegular';
+  font-size: 14px;
+  color: #4a4a4a;
+  align-self: center;
+`;
+
+class BubbleRating extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rating: this.props.rating,
+      total: this.props.total
+    };
+  }
+
+  render() {
+//4.5,5: Excellent
+//3.5,4: Very Good
+//2.5,3: Average
+//1.5,2: Poor
+//1-:Terrible
+    var rating = this.state.rating;
+    var meaning;
+    if (rating >= 4.5) {
+      meaning = "Excellent";
+    } else if (rating >= 3.5) {
+      meaning = "Excellent";
+    } else if (rating >= 2.5) {
+      meaning = "Average";
+    } else if (rating >= 1.5) {
+      meaning= "Poor";
+    } else {
+      meaning = "Terrible";
+    }
+    return (
+      <RatingBox>
+        <RatingMeaning>{meaning}</RatingMeaning>
+        <RatingBubbles>
+          <RatingBubble bubbleColor='#fff' bubbleBackground='#00aa6c' rating={this.state.rating} />
+          <RatingTotal>{this.state.total} reviews</RatingTotal>
+        </RatingBubbles>
+      </RatingBox>
+    );
+  }
+}
+
+export default BubbleRating;
---
+++ client/src/styledComponents/WebsiteLink.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+
+const Link = styled.a`
+  font-family: 'LatoRegular';
+  font-size: 14px;
+  color: #4a4a4a;
+  &:hover {
+    text-decoration: underline;
+    cursor: pointer;
+  }
+`;
+
+const WebsiteLink = (props) => {
+  const [clicked, setClicked] = useState(false);
+
+  useEffect(() => {
+    if (clicked) {
+      window.location.assign(props.link);
+    }
+  });
+
+  return (
+    <Link onClick={() => setClicked(true)}>Visit hotel website</Link>
+  );
+};
+
+export default WebsiteLink;
